72da49fb28f17bc303b62c8b4ab3a5f06f97454d
Michael Snider <msilvprog7@gmail.com>
2025-08-19 13:41:39 -0700

    Add auto-complete fields for activity and feeling suggestions

--- app/app/src/main/java/app/wellbeingquest/ui/theme/Text.kt
+++ app/app/src/main/java/app/wellbeingquest/ui/theme/Text.kt
@@ -5,16 +5,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.InputChip
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -25,6 +34,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.PopupProperties
 
 @Composable
 fun GroupText(text: String, modifier: Modifier) {
@@ -52,17 +62,87 @@ fun GroupLabel(text: String, modifier: Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MultiEntryTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
+fun AutoCompleteTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
     label: @Composable () -> Unit,
     placeholder: @Composable () -> Unit,
-    entries: List<String>,
-    onDone: () -> Unit,
-    onRemove: (String) -> Unit,
-    enabled: Boolean = true,
+    suggestions: List<String>,
+    onSuggestionsChange: (List<String>) -> Unit,
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    onDone: (String) -> Unit = {}
 ) {
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = onExpandedChange
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                onTextChange(it)
+            },
+            label = label,
+            placeholder = placeholder,
+            modifier = modifier
+                .menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+            singleLine = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                onDone(text)
+            })
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { onExpandedChange(false) },
+            properties = PopupProperties(focusable = false)
+        ) {
+            suggestions.forEach { suggestion ->
+                DropdownMenuItem(
+                    text = { Text(suggestion) },
+                    onClick = {
+                        onTextChange(suggestion)
+                        onExpandedChange(false)
+                        onDone(suggestion)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MultiEntryTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+    label: @Composable () -> Unit,
+    placeholder: @Composable () -> Unit,
+    suggestions: List<String>,
+    onSuggestionsChange: (List<String>) -> Unit,
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    selected: List<String>,
+    onSelectedChange: (List<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var onDone: (String) -> Unit = {
+        if (it.isNotBlank() && !selected.contains(it.trim())) {
+            // Add to list
+            onSelectedChange(selected + it.trim())
+
+            // Empty text
+            onTextChange("")
+        }
+    }
+
     Column(modifier = Modifier.padding(16.dp)) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -70,34 +150,32 @@ fun MultiEntryTextField(
                 .fillMaxWidth()
                 .padding(0.dp)
         ) {
-            TextField(
-                value = value,
-                onValueChange = onValueChange,
+            AutoCompleteTextField(
+                text = text,
+                onTextChange = onTextChange,
                 label = label,
                 placeholder = placeholder,
-                singleLine = true,
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = {
-                    onDone()
-                })
+                suggestions = suggestions.filterNot { selected.contains(it) },
+                onSuggestionsChange = onSuggestionsChange,
+                expanded = expanded,
+                onExpandedChange = onExpandedChange,
+                modifier = modifier,
+                onDone = onDone
             )
 
             NavigationButton(
                 imageVector = Icons.Default.Add,
                 contentDescription = "add feeling",
                 onClick = {
-                    onDone()
+                    onDone(text)
                 },
-                enabled = enabled,
+                enabled = text.isNotBlank() && !selected.contains(text.trim()),
                 modifier = Modifier.padding(start = 8.dp)
             )
         }
 
-        FlowRow(
-            // mainAxisSpacing = 8.dp,
-            // crossAxisSpacing = 8.dp
-        ) {
-            entries.forEach { entry ->
+        FlowRow {
+            selected.forEach { entry ->
                 InputChip(
                     selected = false,
                     onClick = {},
@@ -107,7 +185,8 @@ fun MultiEntryTextField(
                             imageVector = Icons.Default.Close,
                             contentDescription = "Remove",
                             modifier = Modifier.clickable {
-                                onRemove(entry)
+                                // Remove
+                                onSelectedChange(selected - entry)
                             }
                         )
                     }
